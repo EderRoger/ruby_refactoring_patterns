1a8ac804df459d2dce0dbbd2cddfc8cdf3cd01e0
Eder Roger <eder.roger.souza@gmail.com>
2015-12-01 21:26:20 -0200

    add methods to show ugly ifs

--- lib/factory/busca.rb
+++ lib/factory/busca.rb
@@ -4,4 +4,24 @@ class Search
     ids = SearchService.search_by(criteria)
     find_by_ids(ids)
   end
+
+  def self.make_criteria(params)
+    criteria = SearchCriteria.new
+    criteria.by_page = params[:results_per_page] || 15 criteria.category = params[:category]
+
+    if params[:search_type] == :promotional
+      criteria.category = :in_promotion
+      criteria.order_by = :most_recently
+    elseif params[:search_type] == :by_category
+      if params[:category]
+        criteria,order_by = params[:order_by] || :most_recently
+      else # back to normal search
+        criteria.order_by = params[:order_by] || revelance
+      end
+    else # normal search
+      criteria.order_by = params[:order_by] || :revelance
+    end
+    criteria
+  end
+
 end
